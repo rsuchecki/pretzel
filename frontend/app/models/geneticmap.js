8e95a38a61974c20a23c750b41ecf8f1d8b1d8e8
Gabriel Keeble-Gagnere <gabriel.keeble-gagnere@ecodev.vic.gov.au>
2017-01-18 15:59:02 +1100

    Interface functionality not needed in full model, now in summary model.

--- frontend/app/models/geneticmap.js
+++ frontend/app/models/geneticmap.js
@@ -7,27 +7,4 @@ export default Model.extend({
   name: attr('string'),
   chromosomes: fragmentArray('chromosome'),
 
-  extraMaps: [],
-  isSelected: false,
-
-  linkTo: Ember.computed('name', function() {
-    return [this.get("id")];
-  }),
-
-  mapDeleteLink: Ember.computed('extraMaps', function() {
-    let exMaps = this.get("extraMaps");
-    let that = this;
-    return exMaps.filter(function(mapid) { return mapid != that.get("id"); });
-  }),
-
-  mapLink: Ember.computed('extraMaps', function() {
-    var retlist = this.get("extraMaps");
-    if (retlist == null) {
-      return [this.get("id")];
-    }
-    else {
-      retlist.push(this.get("id"));
-      return retlist;
-    }
-  })
 });
